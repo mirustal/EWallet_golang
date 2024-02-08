2c4c07583b02483484eb1ff316113054248c2242
Miroslav <guresq@gmail.com>
2024-02-08 15:46:58 +0300

    add readConfig and yml file

--- pkg/configs/configs.go
+++ pkg/configs/configs.go
@@ -1,12 +1,37 @@
 package configs
 
+import (
+	"sync"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type ConfigFiber struct {
-	Type string  `yaml:"type"`
+	Type string `yaml:"type"`
 }
 
 type ConfigMongoDB struct {
-	Host string `json:"host"`
-	Port string `json:"port"`
-	Database string `json:"database"`
+	Host       string `json:"host"`
+	Port       string `json:"port"`
+	Database   string `json:"database"`
 	Collection string `json:"collection"`
+}
+
+type Config struct {
+	IsDebug *bool `yaml:"is_debug"`
+	Listen  ConfigFiber
+	MongoDB ConfigMongoDB
+}
+
+var instance *Config
+var once sync.Once
+
+func GetConfig() *Config {
+	once.Do(func() {
+		instance = &Config{}
+		if err := cleanenv.ReadConfig("./config.yml", instance); err != nil {
+			cleanenv.GetDescription(instance, nil)
+		}
+	})
+	return instance
 }
